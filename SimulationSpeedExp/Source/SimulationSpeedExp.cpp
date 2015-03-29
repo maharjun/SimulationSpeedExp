2e0b1d37022d2246943a456c3775f22e56af423b
maharjun <arjun210493@gmail.com>
2015-03-29 21:07:24 +0530

    STDP Experiment with Uncached CSort
    In this i have sorted the Spike Structs according to (NStart, NEnd) during each time a SpikeQueue Vector is processed so that Cache misses could be minimized.
    
    Only one word for this. DISASTER. Here are the results. they speak quite well for themselves.
    
    Time Spent in Counting Sort = 80320 milliseconds
    Time Spent in Current Processing = 32615 milliseconds
    Time Spent in Spike Storing = 34942 milliseconds
    Just 4 Kicks: 00
    no of spikes estimated = 3139603968
    no of spikes stored = 3139604000
    Dis Shit Tuk 148436 ms
    
    We see that Current Processing + Spike Storing didnt change much (expected since the spikes were pretty non-contiguous even after sorting) but instead we have the HUGE overhead for CountingSort (which has to be applied twice one for NEnd and one for NStart).
    
    So Basically.... The previous 80 odd seconds is the best you can get while engaging STDP.

--- SimulationSpeedExp/Source/SimulationSpeedExp.cpp
+++ SimulationSpeedExp/Source/SimulationSpeedExp.cpp
@@ -34,7 +34,56 @@ inline void prefetch(const char *Pointer, int Size){
 	}
 }
 
+void CountingSortNormalNStart(MexVector<pseudoSynRef> &VectorToBeSorted,
+	MexVector<pseudoSynRef> &OutVector,
+	MexVector<int> &EndIndexVect,
+	int Range){
 
+	for (int j = 0; j < Range; ++j){
+		EndIndexVect[j] = 0;
+	}
+	MexVector<int> StartIndexVect(Range, 0);
+	size_t nElems = VectorToBeSorted.size();
+	for (int j = 0; j < nElems; ++j){
+		EndIndexVect[VectorToBeSorted[j].NStart-1]++;
+	}
+	for (int j = 1; j < Range; ++j){
+		EndIndexVect[j] += EndIndexVect[j - 1];
+		StartIndexVect[j] = EndIndexVect[j - 1];
+	}
+	if (OutVector.capacity() != VectorToBeSorted.capacity()) OutVector.reserve(VectorToBeSorted.capacity());
+	if (OutVector.size() != nElems) OutVector.resize(nElems);
+	for (int j = 0; j < nElems; ++j){
+		int CurrKey = VectorToBeSorted[j].NStart - 1;
+		OutVector[StartIndexVect[CurrKey]] = VectorToBeSorted[j];
+		StartIndexVect[CurrKey]++;
+	}
+}
+void CountingSortNormalNEnd(MexVector<pseudoSynRef> &VectorToBeSorted,
+	MexVector<pseudoSynRef> &OutVector,
+	MexVector<int> &EndIndexVect,
+	int Range){
+
+	for (int j = 0; j < Range; ++j){
+		EndIndexVect[j] = 0;
+	}
+	MexVector<int> StartIndexVect(Range, 0);
+	size_t nElems = VectorToBeSorted.size();
+	for (int j = 0; j < nElems; ++j){
+		EndIndexVect[VectorToBeSorted[j].NEnd - 1]++;
+	}
+	for (int j = 1; j < Range; ++j){
+		EndIndexVect[j] += EndIndexVect[j - 1];
+		StartIndexVect[j] = EndIndexVect[j - 1];
+	}
+	if (OutVector.capacity() != VectorToBeSorted.capacity()) OutVector.reserve(VectorToBeSorted.capacity());
+	if (OutVector.size() != nElems) OutVector.resize(nElems);
+	for (int j = 0; j < nElems; ++j){
+		int CurrKey = VectorToBeSorted[j].NEnd - 1;
+		OutVector[StartIndexVect[CurrKey]] = VectorToBeSorted[j];
+		StartIndexVect[CurrKey]++;
+	}
+}
 void CountingSortCached(MexVector<pseudoSyn> &VectorToBeSorted,
 	//MexVector<int> &SortCompartmentVector,
 	MexVector<pseudoSyn> &OutVector,
@@ -217,7 +266,7 @@ void SerialBinning(
 		TotalSpikes += CurrentQSize;
 		CurrentQueue = NextQueue;
 	}
-	cout << "Number of actual iterations = " << TotalSpikesTemp;
+	std::cout << "Number of actual iterations = " << TotalSpikesTemp;
 }
 
 void CachedBinning(
@@ -243,25 +292,45 @@ void CachedBinning(
 	TotalSpikes = 0;
 	//size_t TotalSpikesTemp = 0;
 	int isdebug = false;
+	MexVector<pseudoSynRef> NEndSortedSpikes, NStartNEndSortedSpikes;
+	MexVector<int> EndIndexVect(N, 0);
+	chrono::time_point<chrono::system_clock> CountingSortTimeBeg, CountingSortTimeEnd;
+	chrono::time_point<chrono::system_clock> CurrentProcingTimeBeg, CurrentProcingTimeEnd;
+	chrono::time_point<chrono::system_clock> SpikeStoreTimeBeg, SpikeStoreTimeEnd;
+	int CountingSortDuration = 0;
+	int CurrentProcingDuration = 0;
+	int SpikeStoreDuration = 0;
+
 	for (int i = 0; i < nSteps; ++i){
 		int temp = i % NCases;
 
 		int CurrentQSize = SpikeQueue[CurrentQueue].size();
 		MexVector<pseudoSynRef>::iterator StartPointer, EndPointer;
+		// performing a normal countingsort without fancy caching
+		CountingSortTimeBeg = chrono::system_clock::now();
+
+		CountingSortNormalNEnd(SpikeQueue[CurrentQueue], NEndSortedSpikes, EndIndexVect, N);
+		CountingSortNormalNStart(NEndSortedSpikes, NStartNEndSortedSpikes, EndIndexVect, N);
+
+		CountingSortTimeEnd = chrono::system_clock::now();
+		CountingSortDuration += chrono::duration_cast<chrono::microseconds>(CountingSortTimeEnd - CountingSortTimeBeg).count();
+
 		if (CurrentQSize){
-			StartPointer = SpikeQueue[CurrentQueue].begin();
-			EndPointer = SpikeQueue[CurrentQueue].end();
+			StartPointer = NStartNEndSortedSpikes.begin();
+			EndPointer = NStartNEndSortedSpikes.end();
 		}
 		else{
 			StartPointer = EndPointer = NULL;
 		}
-
+		CurrentProcingTimeBeg = chrono::system_clock::now();
 		for (MexVector<pseudoSynRef>::iterator iSpike = StartPointer;
 			iSpike < EndPointer; ++iSpike){
 			int temp = *(iSpike->WeightPtr);
 			Iin[iSpike->NEnd - 1] += temp;
 			*(iSpike->WeightPtr) = -temp + Iin[iSpike->NEnd - 1];
 		}
+		CurrentProcingTimeEnd = chrono::system_clock::now();
+		CurrentProcingDuration += chrono::duration_cast<chrono::microseconds>(CurrentProcingTimeEnd - CurrentProcingTimeBeg).count();
 
 		TotalSpikes += CurrentQSize;
 		SpikeQueue[CurrentQueue].clear();
@@ -271,6 +340,7 @@ void CachedBinning(
 		MexVector<__m128i> BinningBuffer(CacheBuffering*nBins);	//each element is 16 bytes
 		MexVector<int> BufferInsertIndex(nBins, 0);
 
+		SpikeStoreTimeBeg = chrono::system_clock::now();
 		for (int j = 0; j < N; ++j){
 			VSim[j] += (Iin[j] - VSim[j]);
 			if (NeuronSelectionVector[temp][j]){
@@ -319,6 +389,8 @@ void CachedBinning(
 			}
 			BufferInsertIndex[i] = 0;
 		}
+		SpikeStoreTimeEnd = chrono::system_clock::now();
+		SpikeStoreDuration += chrono::duration_cast<chrono::microseconds>(SpikeStoreTimeEnd - SpikeStoreTimeBeg).count();
 		CurrentQueue = NextQueue;
 	}
 	for (int i = 0; i < nBins; ++i){
@@ -326,7 +398,10 @@ void CachedBinning(
 		TotalSpikes += SpikeQueue[CurrentQueueTemp].size();
 	}
 	
-	//cout << "Number of actual iterations = " << TotalSpikesTemp << endl;
+	std::cout << "Time Spent in Counting Sort = " << CountingSortDuration / 1000 << " milliseconds" << endl;
+	std::cout << "Time Spent in Current Processing = " << CurrentProcingDuration / 1000 << " milliseconds" << endl;
+	std::cout << "Time Spent in Spike Storing = " << SpikeStoreDuration / 1000 << " milliseconds" << endl;
+
 }
 int main(){
 	mt19937 RandGen;
@@ -413,14 +488,14 @@ int main(){
 			}
 		}
 	}
-	cout << "Just 4 Kicks: " << pseudoV[1] << pseudoIin[1] << endl;
+	std::cout << "Just 4 Kicks: " << pseudoV[1] << pseudoIin[1] << endl;
 	TotalSpikesEstimate = (float)(TotalSpikesEstimate) / 8 * nSteps;
 	if (WeightQueue.size() == Range){
 		for (int i = 0; i < Range; ++i)
-			cout << WeightQueue[i].capacity() << endl;
+			std::cout << WeightQueue[i].capacity() << endl;
 	}
-	cout << "no of spikes estimated = " << TotalSpikesEstimate << endl;
-	cout << "no of spikes stored = " << TotalSpikes << endl;
-	cout << "Dis Shit Tuk " << chrono::duration_cast<chrono::milliseconds>(TEnd - TBeg).count() << " ms" << endl;
-	system("pause");
+	std::cout << "no of spikes estimated = " << TotalSpikesEstimate << endl;
+	std::cout << "no of spikes stored = " << TotalSpikes << endl;
+	std::cout << "Dis Shit Tuk " << chrono::duration_cast<chrono::milliseconds>(TEnd - TBeg).count() << " ms" << endl;
+	std::system("pause");
 }
